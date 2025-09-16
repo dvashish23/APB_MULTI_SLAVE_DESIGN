82c9c5175581e9bd74c337271b8c41be63fb8510
Ashish Saroj <ashishsaroj607@gmail.com>
2025-09-16 14:16:40 +0530

    Create mod_n_cntr.v
    craeted mod-N counter for pready delay

---
+++ rtl/mod_n_cntr.v
@@ -0,0 +1,31 @@
+module mod_n_cntr #(parameter N=1)
+                     (
+                       input clk,
+                       input rstn,
+                       output [4:0] cnt
+                     );
+
+  reg [4:0]cnt_r;
+  assign cnt = cnt_r;
+
+  //MOD - N counter logic 
+  always@(posedge clk or negedge rstn)
+    begin
+      if(!rstn) // active low
+        begin
+          cnt_r <= {5{1'h0}};
+        end
+      else
+        begin
+          if(cnt_r < N)
+            begin
+              cnt_r <= cnt_r + 1'b1;
+            end
+          else
+            begin
+              cnt_r <= 1'b0;
+            end
+        end
+    end
+
+endmodule
